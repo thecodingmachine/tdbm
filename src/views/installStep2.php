574b32e0816768458333f3ff7d1d94811dbf6436
David Négrier <david@davidtcm>
2012-11-28 23:51:35 +0100

    Improving installer design

--- src/views/installStep2.php
+++ src/views/installStep2.php
@@ -1,25 +1,45 @@
-<?php /* @var $this TdbmInstallController */ ?>
+<?php 
+/* @var $this Mouf\Database\TDBM\Controllers\TdbmInstallController */ ?>
 <h1>Setting up TDBM</h1>
 
 <p>By clicking the link below, you will automatically generate DAOs and Beans for TDBM. These beans and DAOs will be written in the directories you specify in the form below.</p>
 
-<form action="generate" method="post">
+<form action="generate" method="post" class="form-horizontal">
 <input type="hidden" id="selfedit" name="selfedit" value="<?php echo plainstring_to_htmlprotected($this->selfedit) ?>" />
 
-<div>
-<label>Source directory:</label><input type="text" name="sourcedirectory" value="<?php echo plainstring_to_htmlprotected($this->sourceDirectory) ?>"></input>
+<div class="control-group">
+	<label class="control-label">Source directory:</label>
+	<div class="controls">
+		<input type="text" name="sourcedirectory" value="<?php echo plainstring_to_htmlprotected($this->sourceDirectory) ?>"></input>
+		<span class="help-block">This is the directory containing your source code (it should be configured in your the "autoload" section of your <em>composer.json</em> file.</span>
+	</div>
 </div>
-<div>
-<label>Dao namespace:</label><input type="text" name="daonamespace" value="<?php echo plainstring_to_htmlprotected($this->daoNamespace) ?>"></input>
+<div class="control-group">
+	<label class="control-label">Dao namespace:</label>
+	<div class="controls">
+		<input type="text" name="daonamespace" value="<?php echo plainstring_to_htmlprotected($this->daoNamespace) ?>"></input>
+		<span class="help-block">The namespace for the DAOs. Be sure to type a namespace that is registered in the "autoload" section of your <em>composer.json</em> file. Otherwise, the composer autoloader will fail to load your classes.</span>
+	</div>
 </div>
-<div>
-<label>Bean namespace:</label><input type="text" name="beannamespace" value="<?php echo plainstring_to_htmlprotected($this->beanNamespace) ?>"></input>
+<div class="control-group">
+	<label class="control-label">Bean namespace:</label>
+	<div class="controls">
+		<input type="text" name="beannamespace" value="<?php echo plainstring_to_htmlprotected($this->beanNamespace) ?>"></input>
+		<span class="help-block">The namespace for the beans. Be sure to type a namespace that is registered in the "autoload" section of your <em>composer.json</em> file. Otherwise, the composer autoloader will fail to load your classes.</span>
+	</div>
 </div>
-<div>
-<label>Keep support for previous DAOs:</label><input type="checkbox" name="keepSupport" value="1"></input>
+<div class="control-group">
+	<label class="control-label">Keep support for previous DAOs:</label>
+	<div class="controls">
+		<input type="checkbox" name="keepSupport" value="1"></input>
+		<span class="help-block">DAOs generated before TDBM 2.3 had a different method signature. This will ensure this signature
+		is respected. Use this only if you are migrating legacy code.</span>
+	</div>
 </div>
 
-<div>
-	<button name="action" value="generate" type="submit">Install TDBM</button>
+<div class="control-group">
+	<div class="controls">
+		<button name="action" value="generate" type="submit" class="btn btn-danger">Install TDBM</button>
+	</div>
 </div>
 </form>
