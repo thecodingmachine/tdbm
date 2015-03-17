10b81a8cdad37773e6a1730777ebaed9db28f385
Guillaume Van Der Putte <g.vanderputte@thecodingmachine.com>
2015-03-17 14:35:20 +0100

    removing type in save and delete functions signature. Split tests, and add a new test for daogeneration. Test daogeneration isn't finish yet.

---
+++ tests/Mouf/Database/TDBM/TDBMDaoGeneratorTest.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ Copyright (C) 2006-2014 David Négrier - THE CODING MACHINE
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+namespace Mouf\Database\TDBM;
+
+use Mouf\Database\DBConnection\MySqlConnection;
+use Mouf\Database\TDBM\Utils\TDBMDaoGenerator;
+use Mouf\Utils\Cache\NoCache;
+use Mouf\Database\TDBM\Filters\EqualFilter;
+use Mouf\Database\TDBM\Filters\OrderByColumn;
+
+// Require needed if we run this class directly
+if (file_exists(__DIR__.'/../../../../../../autoload.php')) {
+	require_once __DIR__.'/../../../../../../autoload.php';
+} else {
+	require_once __DIR__.'/../../../../vendor/autoload.php';
+}
+
+/**
+ */
+class TDBMDaoGeneratorTest extends TDBMAbsctractServiceTest {
+
+    /** @var TDBMDaoGenerator $tdbmDaoGenerator */
+    protected $tdbmDaoGenerator;
+
+    protected function setUp() {
+        $this->tdbmDaoGenerator = new TDBMDaoGenerator($this->dbConnection);
+    }
+
+    /**
+     * @depends testFake
+     */
+	public function testDaoGeneration() {
+        $daoFactoryClassName = "daoFactory";
+        $sourcedirectory = "Mouf/Database/TDBM/Dao";
+        $daonamespace = "Mouf/Database/TDBM/Dao/";
+        $beannamespace = "Mouf/Database/TDBM/Dao/Bean/";
+        $support = false;
+        $storeInUtc = false;
+
+		return $this->tdbmDaoGenerator->generateAllDaosAndBeans($daoFactoryClassName, $sourcedirectory, $daonamespace, $beannamespace, $support, $storeInUtc);
+    }
+
+}
+
+?>
