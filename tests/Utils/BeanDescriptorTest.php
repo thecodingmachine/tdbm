625529e969948761493e492d6fd712a8ed602efe
David NÃ©grier <d.negrier@thecodingmachine.com>
2017-06-29 12:08:24 +0200

    Moving beanNamespace to bean descriptor class.

--- tests/Utils/BeanDescriptorTest.php
+++ tests/Utils/BeanDescriptorTest.php
@@ -54,7 +54,7 @@ class BeanDescriptorTest extends TDBMAbstractServiceTest
     public function testConstructor()
     {
         $usersTable = $this->schema->getTable('users');
-        $beanDescriptor = new BeanDescriptor($usersTable, $this->schemaAnalyzer, $this->schema, $this->tdbmSchemaAnalyzer, new DefaultNamingStrategy(), new VoidListener());
+        $beanDescriptor = new BeanDescriptor($usersTable, 'Tdbm\\Test\\Beans', 'Tdbm\\Test\\Beans\\Generated', $this->schemaAnalyzer, $this->schema, $this->tdbmSchemaAnalyzer, new DefaultNamingStrategy());
         $propertyDescriptors = $beanDescriptor->getBeanPropertyDescriptors();
         $firstElem = reset($propertyDescriptors);
         $idProperty = $propertyDescriptors['$id'];
@@ -70,7 +70,7 @@ class BeanDescriptorTest extends TDBMAbstractServiceTest
     public function testGetConstructorProperties()
     {
         $usersTable = $this->schema->getTable('users');
-        $beanDescriptor = new BeanDescriptor($usersTable, $this->schemaAnalyzer, $this->schema, $this->tdbmSchemaAnalyzer, new DefaultNamingStrategy(), new VoidListener());
+        $beanDescriptor = new BeanDescriptor($usersTable, 'Tdbm\\Test\\Beans', 'Tdbm\\Test\\Beans\\Generated', $this->schemaAnalyzer, $this->schema, $this->tdbmSchemaAnalyzer, new DefaultNamingStrategy());
         $constructorPropertyDescriptors = $beanDescriptor->getConstructorProperties();
         $this->assertArrayHasKey('$name', $constructorPropertyDescriptors);
         // password is nullable
@@ -82,7 +82,7 @@ class BeanDescriptorTest extends TDBMAbstractServiceTest
     public function testGetTable()
     {
         $usersTable = $this->schema->getTable('users');
-        $beanDescriptor = new BeanDescriptor($usersTable, $this->schemaAnalyzer, $this->schema, $this->tdbmSchemaAnalyzer, new DefaultNamingStrategy(), new VoidListener());
+        $beanDescriptor = new BeanDescriptor($usersTable, 'Tdbm\\Test\\Beans', 'Tdbm\\Test\\Beans\\Generated', $this->schemaAnalyzer, $this->schema, $this->tdbmSchemaAnalyzer, new DefaultNamingStrategy());
         $this->assertSame($usersTable, $beanDescriptor->getTable());
     }
 
