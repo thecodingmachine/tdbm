e92bc0da3603d3fe6d711cb3718f111fbda767a7
David Négrier <d.negrier@thecodingmachine.com>
2017-08-29 14:13:13 +0200

    Fixing date format for Oracle

--- tests/TDBMAbstractServiceTest.php
+++ tests/TDBMAbstractServiceTest.php
@@ -109,6 +109,7 @@ abstract class TDBMAbstractServiceTest extends \PHPUnit_Framework_TestCase
                 $evm = new EventManager();
                 $evm->addEventSubscriber(new OracleSessionInit(array(
                     'NLS_TIME_FORMAT' => 'HH24:MI:SS',
+                    'NLS_DATE_FORMAT' => 'YYYY-MM-DD HH24:MI:SS',
                 )));
 
                 $connectionParams = array(
