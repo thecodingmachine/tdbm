7d6857b90efa21e1482222c6377efa5446b10af2
kevin <k.nguyen@thecodingmachine.com>
2021-02-09 06:38:17 +0100

    More precise NoBeanFoundException information

--- src/NoBeanFoundException.php
+++ src/NoBeanFoundException.php
@@ -8,4 +8,55 @@ namespace TheCodingMachine\TDBM;
  */
 class NoBeanFoundException extends TDBMException
 {
+
+    private string $tableName;
+    private string $className;
+
+    /**
+     * @var string[]
+     */
+    private array $primaryKeys;
+
+    public static function missPrimaryKeyRecord(string $tableName, array $primaryKeys, string $className, $previous) : self
+    {
+        $primaryKeysStringified = implode(' and ', array_map(function ($key, $value) {
+            return "'".$key."' = ".$value;
+        }, array_keys($primaryKeys), $primaryKeys));
+        $exception = new self("No result found for query on table '".$tableName."' for ".$primaryKeysStringified, 0, $previous);
+        $exception->tableName = $tableName;
+        $exception->primaryKeys = $primaryKeys;
+        $exception->className = $className;
+
+        return $exception;
+    }
+
+    public static function missFilterRecord(string $tableName) : self
+    {
+        return new self("No result found for query on table '".$tableName."'");
+    }
+
+    /**
+     * @return string
+     */
+    public function getTableName(): string
+    {
+        return $this->tableName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClassName(): string
+    {
+        return $this->className;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getPrimaryKeys(): array
+    {
+        return $this->primaryKeys;
+    }
+
 }
--- src/TDBMService.php
+++ src/TDBMService.php
@@ -1251,10 +1251,7 @@ class TDBMService
         try {
             return $this->findObjectOrFail($table, $primaryKeys, [], $additionalTablesFetch, $className);
         } catch (NoBeanFoundException $exception) {
-            $primaryKeysStringified = implode(' and ', array_map(function ($key, $value) {
-                return "'".$key."' = ".$value;
-            }, array_keys($primaryKeys), $primaryKeys));
-            throw new NoBeanFoundException("No result found for query on table '".$table."' for ".$primaryKeysStringified, 0, $exception);
+            throw NoBeanFoundException::missPrimaryKeyRecord($table, $primaryKeys, $this->getBeanClassName($table), $exception);
         }
     }
 
