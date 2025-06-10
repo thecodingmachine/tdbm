c8371c34dd8d248d99aac3ae02365c58ab3a8cc0
Guillaume <g.alabre@thecodingmachine.com>
2025-06-10 14:14:52 +0200

    :building_construction: Drop support for PHP7.4
    It is not maintained and failing due to some dependencies

--- tests/phpunit-oracle.sh
+++ tests/phpunit-oracle.sh
@@ -6,5 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ..
 
+# TODO upgrade to php8.0
 docker run --rm --name oracle-tdbm-test -v $(pwd):/app moufmouf/oracle-xe-php:7.4-11g vendor/bin/phpunit -c phpunit.oracle.xml
 
