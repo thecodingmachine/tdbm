a96b9d8eb0519111874892c8d594836af0f48678
Guillaume <g.alabre@thecodingmachine.com>
2025-06-10 11:18:37 +0200

    :rotating_light: Fix codestyle and upgrade to phpstan 2.0

--- tests/Utils/DbalUtilsTest.php
+++ tests/Utils/DbalUtilsTest.php
@@ -17,6 +17,6 @@ class DbalUtilsTest extends TestCase
             'key4' => 1,
         ];
 
-        $this->assertSame(['key2'=>Connection::PARAM_INT_ARRAY, 'key3'=>Connection::PARAM_STR_ARRAY, 'key4'=>ParameterType::INTEGER], DbalUtils::generateTypes($params));
+        $this->assertSame(['key2' => Connection::PARAM_INT_ARRAY, 'key3' => Connection::PARAM_STR_ARRAY, 'key4' => ParameterType::INTEGER], DbalUtils::generateTypes($params));
     }
 }
